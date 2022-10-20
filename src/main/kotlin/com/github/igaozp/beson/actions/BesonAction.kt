ac220f45d77b384d7ed6775a970f8815515fc3b8
igaozp <igaozp@hotmail.com>
2022-10-20 13:21:22 +0800

    feat: Hello Beson

---
+++ src/main/kotlin/com/github/igaozp/beson/actions/BesonAction.kt
@@ -0,0 +1,14 @@
+package com.github.igaozp.beson.actions
+
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class BesonAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        Notifications.Bus.notify(Notification("Print", "Beson", "Hello Beson",
+            NotificationType.INFORMATION), e.project)
+    }
+}
